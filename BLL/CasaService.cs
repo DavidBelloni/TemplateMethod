fa40e3ac154a17beb0117145e35c0550670ef216
David Joel Belloni <DavidJoel.Belloni@Alumnos.uai.edu.ar>
2026-08-13 23:09:02 -0300

    Prueba

---
+++ BLL/CasaService.cs
@@ -0,0 +1,19 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class CasaService : ICasaService
+    {
+        public void ConstruirCasa(Casa casa) 
+        {
+
+        }
+    }
+}
+
+// Prueba
